a0d4331a84b364d86a1d5c585a0decae998b0a92
varnikapv <pvarnika05@gmail.com>
2025-11-28 23:49:36 +0530

    day8 product of array

--- arrays/day8/productofarray.py
+++ arrays/day8/productofarray.py
@@ -2,3 +2,14 @@
 # description: Given an array nums of n integers where n > 1, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
 # difficulty: Medium
 
+res = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
